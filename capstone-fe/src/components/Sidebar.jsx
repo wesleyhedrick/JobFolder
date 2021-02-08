4adf1fa899fa920a5080ace94dff69638d9535f9
GSWes <wesley.hedrick@gmail.com>
2021-02-08 18:11:22 -0500

    database calls getting basic info

--- capstone-fe/src/components/Sidebar.jsx
+++ capstone-fe/src/components/Sidebar.jsx
@@ -4,18 +4,21 @@ import {
 
 const navBarItems = [
     {value: 'job-tracker', content: 'Job Tracker'},
-    {value: 'resumes', content: 'Resumes'},
-    {value: 'letters', content: 'Letters'},
+    {value: 'resume', content: 'Resumes'},
+    {value: 'thank-you', content: 'Thank You Letters'},
+    {value: 'cover', content: 'Cover Letters'},
+    // {value: 'templates', content: 'Templates'},
+    // {value: 'calendar', content: 'Calendar'}
     {value: 'interview-questions', content: 'Interview Questions'},
-    {value: 'templates', content: 'Templates'},
-    {value: 'calendar', content: 'Calendar'}
 ]    
 
 function Sidebar({getSummaryData}) {
     return (
         <nav>
             <h1>.jobfolder</h1>
-            {navBarItems.map(item => <div onClick={(e)=>getSummaryData(e)} className={item.value}>{item.content}</div>)}
+            {navBarItems.map(item => 
+                <div onClick={(e)=>getSummaryData(e)} className={item.value}>{item.content}</div>)
+            }
         </nav>
     )
 }
