01fc7e64679e00d35507fb33759c27618d3e6d90
mculep <mculep@gmail.com>
2021-02-14 18:41:26 -0800

    2nd push for dashboard

--- capstone-fe/src/components/Sidebar.jsx
+++ capstone-fe/src/components/Sidebar.jsx
@@ -38,7 +38,8 @@ function Sidebar({id, getSummaryData}) {
     <>
         <div className="sidebar-container">
         <h1 className="sidebar-title">.jobfolder</h1>
-                <div className="sidebar">
+        <div className="sidebar">
+                
                 {navBarItems.map(item => 
                     <div onClick={(e)=>getSummaryData(e)} className={item.value}> <p className="icons">{item.icon}</p><p className="link-items">{item.content}</p></div>)
                 }
